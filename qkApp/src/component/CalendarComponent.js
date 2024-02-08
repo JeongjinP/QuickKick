4063e4b9b7c66718dd350d21d37e5c00e0381c65
근찬 <rmscks97@inu.ac.kr>
2024-02-09 00:31:18 +0900

    주석추가

--- qkApp/src/component/CalendarComponent.js
+++ qkApp/src/component/CalendarComponent.js
@@ -1,6 +1,9 @@
 import React, { useState }  from "react";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 
+// 외부 라이브러리 react-native-calendars 를 사용하여 달력 컴포넌트를 만듬
+// https://github.com/wix/react-native-calendars
+
 LocaleConfig.locales["kr"] = {
   monthNames: [
     "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월",],

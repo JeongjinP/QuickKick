f25c30678e71e2a325cf8747d73c735ac1e30b5c
근찬 <rmscks97@inu.ac.kr>
2024-02-14 14:29:37 +0900

    더미 파일 제거

--- qkApp/src/component/LoginTestComponent.js
+++ qkApp/src/component/LoginTestComponent.js
@@ -10,23 +10,23 @@ import axios from "axios";
   Backend 단에서 Spring boot를 통해서 DB 접근
 */
 
-<<<<<<< HEAD
-
-useEffect(async() => {
-  try {
-    const res = await axios.get('')
-    const _inputData = await res.data.map((rowData)=> ({
-      ID: rowData.ID,
-      PW: rowData.password,
-      name: rowData.stdName
-      })
-    )
-    setInputData(inputData.concat(_inputData))
-  } catch(e) {
-    console.error(e.message)
-  }
-},[]);
-=======
+// <<<<<<< HEAD
+//
+// useEffect(async() => {
+//   try {
+//     const res = await axios.get('')
+//     const _inputData = await res.data.map((rowData)=> ({
+//       ID: rowData.ID,
+//       PW: rowData.password,
+//       name: rowData.stdName
+//       })
+//     )
+//     setInputData(inputData.concat(_inputData))
+//   } catch(e) {
+//     console.error(e.message)
+//   }
+// },[]);
+// =======
 // 테스트용 데이터
 const dummyData = {
   ID: "2024",
@@ -34,14 +34,14 @@ const dummyData = {
   NAME:"이근찬",
   TEAM:"사상 최강의 팀"
 }
->>>>>>> 08b8100b8ea9840fe0c3b6e6e404c20738e36071
+// >>>>>>> 08b8100b8ea9840fe0c3b6e6e404c20738e36071
 
 export function LoginTestComponent( props ) {
-  const baseUrl = "http://localhost:8080";
+  // const baseUrl = "http://localhost:8080";
 
   // props 로 받은 ID, PW를 dummyData 와 비교하여 로그인 성공 여부와 성공시 유저 이름을 반환
   const checkUser = (id, pw) => {
-    if(id === rowData.ID && pw === rowData.PW){
+    if(id === dummyData.ID && pw === dummyData.PW){
       console.log("로그인 성공");
 
       // 성공여부, 유저이름, 유저팀 반환

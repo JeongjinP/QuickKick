c9a191ee234d9b027c773eed2c79e56b7a792419
근찬 <rmscks97@inu.ac.kr>
2024-02-16 19:31:19 +0900

    예약 신청서 화면 필요사항 정리

--- qkApp/src/screens/reserve/ReservationReport.js
+++ qkApp/src/screens/reserve/ReservationReport.js
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import ReserveHeader from "./ReserveHeader";
 
+// 신청자명, 건물명, 전화번호, 운동장, 이메일, 신청일자, 소속(단과대), 사용인원, 예약시간,
+// 사용목적(입력), 사용내용(입력), 계획서첨부(?)
 
 function ReservationReport () {
   return (

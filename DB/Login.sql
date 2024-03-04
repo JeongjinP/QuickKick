b2ef36ce739e374ca6fa6f89f374ccbaa313bcd1
JeongjinP <rednoy03@gmail.com>
2024-03-04 15:02:59 +0900

    DB changed

---
+++ DB/Login.sql
@@ -0,0 +1,9 @@
+insert into Login values (201700309, 'wjdwlsl12!', '박정진');
+insert into Login values (202101560, '12341234!', '조성현');
+select * from Login where id = 201700309 and password = 'wjdwlsl12!';
+
+select * from Login;
+
+describe Login;
+
+select * from member where teamName = '사상 최강의 팀';

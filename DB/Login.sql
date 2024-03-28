2e6228a351ec21033a58a6e00b1a0c15bc392b50
근찬 <rmscks97@inu.ac.kr>
2024-03-28 15:43:52 +0900

    발표용 내용 수정

--- DB/Login.sql
+++ DB/Login.sql
@@ -1,5 +1,6 @@
 insert into Login values (201700309, 'wjdwlsl12!', '박정진');
 insert into Login values (202101560, '12341234!', '조성현');
+
 select * from Login where id = 201700309 and password = 'wjdwlsl12!';
 
 select * from Login;
--- qkApp/src/component/LoginTestComponent.js
+++ qkApp/src/component/LoginTestComponent.js
@@ -1,31 +1,29 @@
-import React from "react";
+// import React from "react";
 import axios from "axios";
 
 
 export function LoginTestComponent(props) {
-
-  // 서버의 URL
+  // 로그인 URL
   const SERVER_URL = "http://localhost:8080/member";
 
-  // ID, PW를 서버에 전송하여 사용자 인증을 시도
+  // ID, PW를 서버에 전송하여 사용자 인증
   const authenticateUser = async (id, pw) => {
     console.log("입력 id:",id);
     console.log("입력 pw:",pw);
     try {
-      // 서버에 POST 요청을 보내기 위한 데이터
+      // 서버에 POST 요청을 보내기 위한 데이터, URL 설정하고 POST 요청
       const data = { id: id, password: pw };
       const url = `${SERVER_URL}/login?id=${data.id}&password=${data.password}`
       const response = await axios.post(url);
-      console.log("응답",response.data);
+
+      // 리턴하는 json 데이터의 이름 std_name 이라고 했는데 실제 받아보면 stdName 인것 확인 해보기
+      console.log("응답받은 데이터",response.data);
       // 서버로부터 받은 응답 처리
       if (response.data.id) {
         console.log("로그인 성공");
-        // 성공 여부, 유저 이름, 유저 팀 반환
-        console.log(response.data.id);
-        console.log(response.data.stdName);
-
-
-
+        // 성공 여부, 유저 이름, (유저 팀)- 추가해야함/  반환
+        // console.log(response.data.id);
+        // console.log(response.data.stdName);
         return { success: true, stdName: response.data.stdName};
       } else {
         console.log("로그인 실패");
@@ -37,6 +35,6 @@ export function LoginTestComponent(props) {
     }
   };
 
-  // props로 받은 ID, PW를 서버에 전달하여 사용자 인증 시도
+  // props 로 받은 ID, PW를 함수에 전달하고 리턴
   return authenticateUser(props.credential.ID, props.credential.PW);
 }
